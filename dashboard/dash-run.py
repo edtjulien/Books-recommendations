4f37b6ffc46167da0d3d6cc25bca4785f2b7b8f4
Julien <julien.defaut@gmail.com>
2022-12-01 15:29:12 +0100

    update dash

--- dashboard/dash-run.py
+++ dashboard/dash-run.py
@@ -8,20 +8,27 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 df = pd.read_json('../output/final/data-books.json', lines=True)
 
 @app.callback(
-    [Output('subtitle', 'children')],
+    [Output('subtitle', 'children'),
+    Output('gender-pie', 'figure')
+    ],
    # [Input('input_search', 'value')],
-    Input('submit-search', 'n_clicks'),
-    State('input-search', 'value')
+    Input('input-search', 'value'),
+    #State('input-search', 'value')
     #Output('map-spot', 'figure'),
 )
-def update_graph(n_clicks, text):
+def update_graph(text):
     if not text:
         return ['']
     df_found = df.loc[df['title'].str.contains(text, case=False)].reset_index()
     title = 'Aucun livre trouvé'
     if df_found.shape[0] > 0:
         title = df_found.loc[0,'title']
-    return [f'Titre du livre: {title}']
+
+    book_id = df_found.loc[0,'book_id']
+
+    fig = px.pie(df[df['book_id'] == book_id], sdlf)
+        
+    return [f'Titre du livre: {title}'], fig
 
 
 app.layout = html.Div(children=[
@@ -30,16 +37,27 @@ app.layout = html.Div(children=[
     dcc.Input(
             id="input-search",
             type='text',
+            value='to',
             placeholder="Chercher un livre",
         ),
-    html.Button('Submit', id='submit-search', n_clicks=0),
+    # dcc.Dropdown(
+    #     options=[
+    #         {'label': 'New York City', 'value': 'NYC'},
+    #         {'label': u'Montréal', 'value': 'MTL'},
+    #         {'label': 'San Francisco', 'value': 'SF'}
+    #     ],
+    #     value=['MTL', 'SF'],
+    #     multi=False,
+    #     id='input'
+    # ),
+    #html.Button('Submit', id='submit-search', n_clicks=0),
 
     html.Div(id='subtitle', children=[], className='sub-title'),
 
-    # dcc.Graph(
-    #     id='map-spot',
-    #     figure={},
-    # ),
+    dcc.Graph(
+        id='gender-pie',
+        figure={},
+    ),
 ])
 
 
